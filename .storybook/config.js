17dabea3d0b19cf331ec9bbdd3ec619a68eb35b3
aziziyazit <aziziyazit@gmail.com>
2019-09-22 21:59:22 +0800

    fix loadStories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/angular';
+const req = require.context('../src/stories', true, /\.stories\.ts$/);
 
 function loadStories() {
-  require('../src/stories/index.ts');
+  req.keys().forEach(req);
 }
 
 configure(loadStories, module);
